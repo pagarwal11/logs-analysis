16df0b1a7587b243647de72a67ad280947540dc4
Priyank Agarwal <PAgarwal11@slb.com>
2018-09-11 16:29:04 +0530

    updated readme file

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # logs-analysis
-Udacity Full Stack Nanodegree Logs Analysis Project 
+
+Udacity Full Stack Nanodegree Logs Analysis Project
 
 The project comprises of a python program to connect to the sample database 'news' and querying it to display some of the desired information.
+The database 'news' has three tables namely log, authors and articles with different schema each storing some specific information.
+The objective wwas to explore this information and then come up with queries to print information such as articles/authors by popularity and days with high number of bad requests.
 
 Content Description:
-1. log - Python file that contains the queries for printing the desired output. More specifically, it queries  the database 'news' to print top three articles and authors of all time and the days on which over 1% of total requests led to errors.
-To see the output, run this python file.
 
-2. logs_output - A text file that shows the output obtained upon running the pyton program. 
+1. log - Python file that contains the queries for printing the desired output. More specifically, it queries the database 'news' to print top three articles and authors of all time and the days on which over 1% of total requests led to errors.
+   To see the output, run this python file.
+
+2. logs_output - A text file that shows the output obtained upon running the pyton program.
--- logs.py
+++ logs.py
@@ -1,49 +1,77 @@
+#!/usr/bin/env python3
+
 # "Database code" for the Logs.
 
 import psycopg2
 
 DBNAME = 'news'
 
-#runs the query passed in parameter and returns the result
-def run_query(query):
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
-    c.execute(query)
-    result = c.fetchall()
-    db.close()
-    return result
+# runs the query passed in parameter and returns the result
 
-#query to find top three articles by popularity
+
+def run_query(query):
+    try:
+        db = psycopg2.connect("dbname=news")
+    except psycopg2.Error as e:
+        print("Unable to connect!")
+        print(e.pgerror)
+        print(e.diag.message_detail)
+        sys.exit(1)
+    else:
+        c = db.cursor()
+        c.execute(query)
+        result = c.fetchall()
+        db.close()
+        return result
+
+# query to find top three articles by popularity
+
+
 def articles_by_popularty():
-    query = 'select title,count(*) as no_of_views from articles,log where log.path LIKE concat(\'%\',articles.slug) group by articles.title order by no_of_views desc limit 3'
+    query = """select title,count(*) as no_of_views from articles,log where
+    log.path LIKE concat(\'%\',articles.slug)
+    group by articles.title order by no_of_views desc limit 3"""
     result = run_query(query)
     print('The top three articles sorted by popularity are: ')
     for title, no_of_views in result:
         print('{} with {} views'.format(title, no_of_views))
     pass
 
-#query to find top three authors by popularity
+# query to find top three authors by popularity
+
+
 def authors_by_popularity():
-    query = """select authors.name,sum(no_of_views) as total_author_views from (select author as distinct_author,count(*) as no_of_views from articles,log where log.path LIKE concat(\'%\',articles.slug) group
-    by articles.title,articles.author) as no_of_views,authors where authors.id = distinct_author group by authors.name order by total_author_views desc""";
+    query = """select authors.name as name,sum(no_of_views) as total_author_views from
+    (select author as distinct_author,count(*) as no_of_views
+    from articles,log where log.path LIKE concat(\'%\',articles.slug)
+    group by articles.title,
+    articles.author) as no_of_views,authors where
+    authors.id = distinct_author group by authors.name order by
+    total_author_views desc"""
     result = run_query(query)
     print('The top authors sorted by popularity are: ')
-    for id in result:
-        print(id)
+    for name, total_author_views in result:
+        print('{} with {} views'.format(name, total_author_views))
     pass
 
-#query to find days on which more than 1% of total requests led to errors
+# query to find days on which more than 1% of total requests led to errors
+
+
 def error_log():
-    query = """select err_date,(err * 100.0 / total) as error_requests from (select date(time) as err_date,count(*) as err from log where log.status = \'404 NOT FOUND\' group by date(time)) as err_requests,
-    (select date(time) as comparison_date,count(*) as total from log group by date(time)) as total_requests
-    where err_date = comparison_date and err > 0.01 * total""";
+    query = """select err_date,(err * 100.0 / total) as error_requests from
+    (select date(time) as err_date,count(*) as err from log where
+    log.status = \'404 NOT FOUND\' group by date(time)) as err_requests,
+    (select date(time) as comparison_date,count(*) as total from log
+    group by date(time)) as total_requests
+    where err_date = comparison_date and err > 0.01 * total"""
     result = run_query(query)
     print('The dates with more than 1% error requests are: ')
-    for err_date,error_requests in result:
+    for err_date, error_requests in result:
         print('{} with {}% error requests'.format(err_date, error_requests))
     pass
 
-if __name__ == "__main__": 
+
+if __name__ == "__main__":
     articles_by_popularty()
     authors_by_popularity()
     error_log()
